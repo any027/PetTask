d2b26f49a21355e9536aa7260a162a4c5569fa09
Tim <timothyk7@gmail.com>
2014-02-08 02:44:56 -0800

    Added imageview and listview

--- PetTask/src/com/pet/task/MainActivity.java
+++ PetTask/src/com/pet/task/MainActivity.java
@@ -1,15 +1,49 @@
 package com.pet.task;
 
 import android.os.Bundle;
+import android.annotation.SuppressLint;
+import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapFactory.Options;
+import android.graphics.Matrix;
+import android.util.DisplayMetrics;
 import android.view.Menu;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends Activity {
-
+	public static int screenW, screenH;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		//initializations
+		DisplayMetrics dm = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(dm);
+		screenW = dm.widthPixels;
+		screenH= dm.heightPixels;
+		RelativeLayout main = (RelativeLayout) findViewById(R.id.main); //main layout
+		initilizeView(main);
+		
+	}
+	
+	/* Create views for app*/
+	protected void initilizeView(RelativeLayout main)
+	{
+		//Placeholder for animated animal
+		ImageView image = (ImageView)findViewById(R.id.pet_main);
+		image.setImageResource(R.drawable.placeholder);
+		//Matrix matrix = new Matrix();
+		//image.setImageMatrix(matrix);
+		
+		//ListView
+		ListView list = (ListView)findViewById(R.id.todo_main);
+		
 	}
 
 	@Override
