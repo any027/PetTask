dbb523cd614c23435e93978e34fc28dd4a56e697
Wendy Tang <wenhuitang0521@gmail.com>
2014-02-20 22:43:46 -0800

    Created a dialog bubble that displays a random message when the app is started.

--- PetTask/src/com/pet/task/MainActivity.java
+++ PetTask/src/com/pet/task/MainActivity.java
@@ -1,9 +1,14 @@
 package com.pet.task;
 
+import java.util.Random;
+
+//import com.example.test.R;
+
 import android.os.Bundle;
 import android.annotation.SuppressLint;
 import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.BitmapFactory.Options;
@@ -17,10 +22,14 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.graphics.*;
 
 public class MainActivity extends Activity {
 	public static int screenW, screenH;
+
+	private String[] messages;  // array of strings
+	Random rand = new Random(); // random number generator
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +42,16 @@ public class MainActivity extends Activity {
 		screenH= dm.heightPixels;
 		RelativeLayout main = (RelativeLayout) findViewById(R.id.main); //main layout
 		initilizeView(main);
+		
+		// fill in string array 
+		Resources res = getResources();
+		messages = res.getStringArray(R.array.messages);
+		
+		// use random number generator to pick a random string from the array
+		String message = messages[rand.nextInt(messages.length)];
+		
+		TextView greeting = (TextView) findViewById(R.id.dialog);
+		greeting.setText(message); // set text as the random message
 		
 	}
 	
